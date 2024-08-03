53b2ae88316279f55f1f4d2ecf0d8bd2d7548c33
MxmDnn <mxmjuk@gmail.com>
2024-08-03 16:20:24 +0300

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -11,7 +11,7 @@ def send_email(message, recipient, *, sender='university.help@gmail.com') :
     if recipient.find('@' and '.com' or '.ru' or '.net') and sender.find('@' and '.com' or '.ru' or '.net') < 0 :
 # В этом случае код 4 - send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
 # выводит Невозможно отправить письмо с адреса urban.teacher@mail.ru на адрес urban.teacher@mail.ru
-# вместо Нельзя отправить письмо самому себе! Остальное выводится правильно. Остальное выводится правильно.
+# вместо Нельзя отправить письмо самому себе! Остальное выводится правильно.
         print('Невозможно отправить письмо с адреса', str(sender), 'на адрес', str(recipient))
     elif recipient == sender :
         print('Нельзя отправить письмо самому себе!')
